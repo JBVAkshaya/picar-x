44c02947399b95010fa0a15cc329548f0dd23c59
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-15 16:15:13 -0800

    park left

--- scripts/robot_actions.py
+++ scripts/robot_actions.py
@@ -15,8 +15,18 @@ def move_straight():
     px = Picarx()
     px.forward(50)
     time.sleep(0.5)
+    px.stop()
+    time.sleep(0.1)
     px.backward(50)
     time.sleep(0.5)
 
+def park_left():
+    px = Picarx()
+    for angle in range(0,10):
+        px.set_dir_servo_angle(angle)
+        time.sleep(0.01)
+
+
 if __name__ == "__main__":
     move_straight()
+    park_left()
