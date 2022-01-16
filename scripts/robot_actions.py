ddaface93e526e32645d43ad085cdf7997d9ca28
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-15 17:19:06 -0800

    improved park left

--- scripts/robot_actions.py
+++ scripts/robot_actions.py
@@ -28,7 +28,7 @@ def park_left():
         px.set_dir_servo_angle(angle)
         time.sleep(0.01)
     px.forward(50)
-    time.sleep(0.2)
+    time.sleep(0.5)
     px.stop()
     time.sleep(1)
     # for angle in range(-15,0):
