a1a70fd291f9039ed0a6c618bac19c462d21263b
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-15 18:22:02 -0800

    take user input

--- scripts/user_control.py
+++ scripts/user_control.py
@@ -9,7 +9,7 @@ datefmt ="% H :% M :% S ")
 
 logging.getLogger().setLevel(logging.DEBUG)
 
-if __init__=="__main__":
+if __name__=="__main__":
     while(1):
         val = input('Enter action: \n To move straight: 8, \n To park right: 6, \n To park left: 4, \n To make K turn: 5')
     logging.debug("Entered val: %d", val)
