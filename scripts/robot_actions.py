0a367e100c262792763f5873204e15dfac93619f
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-15 16:02:18 -0800

    Move forward and backward

---
+++ scripts/robot_actions.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(1, '../../picar-x/lib')
+from picarx_improved import Picarx
+import time
+
+import logging 
+
+logging_format = "%(asctime)s : %(message)s"
+logging.basicConfig ( format = logging_format , level = logging.INFO ,
+datefmt ="% H :% M :% S ")
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+def move_straight():
+    px = Picarx()
+    px.forward(50)
+    time.sleep(0.2)
+    px.backward(50)
+    time.sleep(0.2)
+
+if __name__ == "__main__":
+    move_straight()
