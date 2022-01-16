abb48f6a24340e9d7ab701b58a482f23c17afe88
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-15 17:31:47 -0800

    park left with stop after each step

--- scripts/robot_actions.py
+++ scripts/robot_actions.py
@@ -22,34 +22,44 @@ def move_straight():
 
 def park_left():
     px = Picarx()
-    px.set_dir_servo_angle(0)
-    time.sleep(0.1)
+
+    ## Turn wheels right
     for angle in range(0,35):
         px.set_dir_servo_angle(angle)
         time.sleep(0.01)
+    
+    ## Move Forward
     px.forward(50)
     time.sleep(0.5)
     px.stop()
-    time.sleep(1)
-    # for angle in range(-15,0):
-    #     px.set_dir_servo_angle(angle)
-    #     time.sleep(0.01)
-    px.set_dir_servo_angle(0)
     time.sleep(0.1)
+
+    ## Straighten wheels and move back
+    for angle in range(35, 0, -1):
+        px.set_dir_servo_angle(angle)
+        time.sleep(0.01)
     px.backward(30)
     time.sleep(1)
     px.stop()
-    time.sleep(1)
-    px.set_dir_servo_angle(25)
     time.sleep(0.1)
+
+    ## Move back while steering to right. Thish will straighten the car
+    for angle in range(0,25):
+        px.set_dir_servo_angle(angle)
+        time.sleep(0.01)
     px.backward(30)
     time.sleep(1.1)
-    px.set_dir_servo_angle(0)
-    time.sleep(0.1)
     px.stop()
-    time.sleep(1)
+    time.sleep(0.1)
+
+    ## Straighten wheel and move in front.
+    for angle in range(25, 0 , -1):
+        px.set_dir_servo_angle(angle)
+        time.sleep(0.01)
     px.forward(20)
     time.sleep(2)
+    px.stop()
+    time.sleep(0.1)
 
 
 
