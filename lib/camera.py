cd1b777e39ba15720da857ecad859bb23f103239
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-22 12:49:19 -0800

    Read camera

--- lib/camera.py
+++ lib/camera.py
@@ -15,12 +15,12 @@ class Camera(object):
 
 if __name__=="__main__":
     cam = Camera()
-    for frame in cam.capture_continuous(cam.rawCapture, format="bgr",use_video_port=True):
+    for frame in cam.camera.capture_continuous(cam.rawCapture, format="bgr",use_video_port=True):
         img = frame.array
         cv2.imshow("video", img)
         cam.rawCapture.truncate(0)
         k = cv2.waitKey(1) & 0xFF
         # 27 is the ESC key, which means that if you press the ESC key to exit
         if k == 27:
-            cam.close()
+            cam.camera.close()
             break
