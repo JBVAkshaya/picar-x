9bd16466ee21df0a101a8e8adb66d5d2ab204ecc
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-22 13:34:15 -0800

    edge detection

--- lib/camera.py
+++ lib/camera.py
@@ -10,14 +10,30 @@ class Camera(object):
         self.camera.framerate = framerate
         self.rawCapture = PiRGBArray(self.camera, size=self.camera.resolution) 
 
+    def region_of_interest(self, edges):
+        height, width = edges.shape
+        mask = np.zeros_like(edges)
+
+        # only focus bottom half of the screen
+        polygon = np.array([[
+            (0, height * 1 / 3),
+            (width, height * 1 / 3),
+            (width, height),
+            (0, height),
+        ]], np.int32)
+
+        cv2.fillPoly(mask, polygon, 255)
+        cropped_edges = cv2.bitwise_and(edges, mask)
+        return cropped_edges
+
     def get_line_edges(self, bgr, h_lower = 60, h_upper = 150):
         hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
         lower_blue = np.array([h_lower, 40, 40])
         upper_blue = np.array([h_upper, 255, 255])
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
         edges = cv2.Canny(mask, 200, 400)
-        # line_edges = 
-        return bgr, mask, edges
+        line_edges = self.region_of_interest(edges)
+        return bgr, mask, line_edges
 
 if __name__=="__main__":
     cam = Camera()
