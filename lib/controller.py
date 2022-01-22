8bb3dbc3c2c81358cf50947f2900dfa8861ae2a2
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-21 18:39:19 -0800

    Controller Class

--- lib/controller.py
+++ lib/controller.py
@@ -15,7 +15,7 @@ class Controller(object):
         The main control method should call the steering-servo method from your car class so that 
         it turns the car toward the line. It should also return the commanded steering angle.
         '''
-        steer_angle = int(offset*steer_angle)
+        steer_angle = int(-offset*steer_angle)
         self.car.set_dir_servo_angle(steer_angle)
         logging.debug(f"Steering Angle: {steer_angle}")
         return steer_angle
@@ -25,7 +25,7 @@ if __name__=="__main__":
     polarity, sensitivity = sensor.calibrate()
     interpret = Interpreter(polarity, sensitivity)
     car = Picarx()
-    controller = Controller(car, interpret.output(sensor.sensor_reading()),10)
+    controller = Controller(car, interpret.output(sensor.sensor_reading()),20)
     while(1):
         sensor_vals = sensor.sensor_reading()
-        controller.control(interpret.output(sensor_vals), 10)
+        controller.control(interpret.output(sensor_vals), 20)
