05ead8c062078b25fd9e2b6b30e5ca8d67e4319a
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-22 16:24:34 -0800

    display lane

--- lib/camera.py
+++ lib/camera.py
@@ -4,6 +4,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
 import logging
+import math
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -113,7 +114,25 @@ class Camera(object):
         line_segments = self.detect_line_segments(edges)
         logging.debug(f"all line segments: {line_segments}")
         lane_lines = self.average_slope_intercept(line_segments)
-        return bgr, mask, edges, lane_lines
+
+        height, width = self.camera.resolution
+        x_offset = 0.0
+        y_offset = 0.0
+        if len (lane_lines) == 2:
+            _, _, left_x2, _ = lane_lines[0][0]
+            _, _, right_x2, _ = lane_lines[1][0]
+            mid = int(width / 2)
+            x_offset = (left_x2 + right_x2) / 2 - mid
+            y_offset = int(height / 2)
+        elif len(lane_lines) == 1:
+            x1, _, x2, _ = lane_lines[0][0]
+            x_offset = x2 - x1
+            y_offset = int(height / 2)
+        
+        angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
+        angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
+        steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
+        return bgr, mask, edges, lane_lines, steering_angle
     
     def display_lines(self, frame, lines, line_color=(0, 255, 0), line_width=2):
         line_image = np.zeros_like(frame)
@@ -127,18 +146,41 @@ class Camera(object):
                     line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
         return line_image
 
+    def display_heading_line(self, frame, steering_angle, line_color=(0, 0, 255), line_width=5 ):
+        heading_image = np.zeros_like(frame)
+        height, width, _ = frame.shape
+
+        # figure out the heading line from steering angle
+        # heading line (x1,y1) is always center bottom of the screen
+        # (x2, y2) requires a bit of trigonometry
+
+        # Note: the steering angle of:
+        # 0-89 degree: turn left
+        # 90 degree: going straight
+        # 91-180 degree: turn right 
+        steering_angle_radian = steering_angle / 180.0 * math.pi
+        x1 = int(width / 2)
+        y1 = height
+        x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
+        y2 = int(height / 2)
+
+        cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
+        heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
+
+        return heading_image
 if __name__=="__main__":
     cam = Camera()
     for frame in cam.camera.capture_continuous(cam.rawCapture, format="bgr",use_video_port=True):
 
-        bgr, mask, edges, lines = cam.detect_lane(frame.array)
+        bgr, mask, edges, lines, steer_angle = cam.detect_lane(frame.array)
         logging.error(f"lines: {lines, len(lines)}")
 
         if len(lines)>0:
             logging.error(f"line cord: {lines[0][0]}")
             line_image = cam.display_lines(bgr,lines)
             cv2.imshow("edges", line_image)
-
+            heading_img = cam.display_heading_line(line_image, steer_angle)
+            cv2.imshow("steer dir", heading_img)
             # 
         cv2.imshow("video", bgr)
         cv2.imshow("mask", mask)
