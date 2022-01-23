5ac03028e5efa716b511993d5a8b7baebe522187
JBVAkshaya <agrawaak@oregonstate.edu>
2022-01-22 16:07:42 -0800

    display lane

--- lib/camera.py
+++ lib/camera.py
@@ -71,43 +71,40 @@ class Camera(object):
             logging.info('No line_segment segments detected')
             return lane_lines
 
-        # height, width, _ = frame.shape
-        # left_fit = []
-        # right_fit = []
-        fit = []
+        height, width = self.camera.resolution
+        left_fit = []
+        right_fit = []
 
-        # boundary = 1/3
-        # left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
-        # right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+        boundary = 1/3
+        left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+        right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
 
         for line_segment in line_segments:
             for x1, y1, x2, y2 in line_segment:
-                # if x1 == x2:
-                #     logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
-                #     continue
-                # fit = np.polyfit((x1, x2), (y1, y2), 1)
-                # slope = fit[0]
-                # intercept = fit[1]
-                # if slope < 0:
-                #     if x1 < left_region_boundary and x2 < left_region_boundary:
-                #         left_fit.append((slope, intercept))
-                # else:
-                #     if x1 > right_region_boundary and x2 > right_region_boundary:
-                #         right_fit.append((slope, intercept))
-                slope, intercept = np.polyfit((x1, x2), (y1, y2), 1)
-                logging.debug(f"slope, intercept: {slope, intercept}")
-                fit.append((slope,intercept))
+                if x1 == x2:
+                    logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                    continue
+                fit = np.polyfit((x1, x2), (y1, y2), 1)
+                slope = fit[0]
+                intercept = fit[1]
+                if slope < 0:
+                    if x1 < left_region_boundary and x2 < left_region_boundary:
+                        left_fit.append((slope, intercept))
+                else:
+                    if x1 > right_region_boundary and x2 > right_region_boundary:
+                        right_fit.append((slope, intercept))
 
-        fit_average = np.average(fit, axis=0)
-        logging.debug(f"len fit, fit avg: {len(fit), fit_average}")
-        if len(fit) > 0:
-            lane_lines.append(self.make_points(fit_average))
+        left_fit_average = np.average(left_fit, axis=0)
+        if len(left_fit) > 0:
+            lane_lines.append(self.make_points(frame, left_fit_average))
 
-        # right_fit_average = np.average(right_fit, axis=0)
-        # if len(right_fit) > 0:
-        #     lane_lines.append(make_points(frame, right_fit_average))
+        right_fit_average = np.average(right_fit, axis=0)
+        if len(right_fit) > 0:
+            lane_lines.append(self.make_points(frame, right_fit_average))
 
-        logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+        logging.debug('lane lines: %s' %
+        lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
 
         return lane_lines
 
